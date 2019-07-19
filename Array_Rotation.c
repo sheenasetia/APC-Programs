9d131a14f9f1ef0fd428e72af73af96257beb5f3
sheenasetia <42132857+sheenasetia@users.noreply.github.com>
2019-07-19 20:50:47 +0530

    Array_Rotation

---
+++ Array_Rotation.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void ArrayRotation(int *arr,int n,int r)
+{
+    r=r%n;
+    for(int i=0;i<r;i++)
+    {
+        int temp=arr[n-1];
+        for(int j=n-1;j>0;j--)
+        {
+            arr[j]=arr[j-1];
+        }
+        arr[0]=temp;
+    }
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int r;
+    scanf("%d",&r);
+    ArrayRotation(arr,n,r);
+    for(int i=0;i<n;i++)
+    {
+        printf("%d",arr[i]);
+    }
+    return 0;
+}
