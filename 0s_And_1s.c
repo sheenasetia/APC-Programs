837a74b8ea6af1ba15887a80876ff6a55937c01e
sheenasetia <42132857+sheenasetia@users.noreply.github.com>
2019-07-28 08:46:57 +0530

    0's_And_1's (leave first row and col method)

--- 0s_And_1s.c
+++ 0s_And_1s.c
@@ -1,9 +1,10 @@
+//if find zero , make its corresponding row and column all zeros
 #include <stdio.h>
 #include <stdlib.h>
 void Zeros_Ones(int n,int arr[n][n])
 {
     int r,c;
-    for(int i=0;i<n;i++)
+    for(int i=0;i<n;i++) //check for zeros in 1st row
     {
         if(arr[0][i]==0)
         {
@@ -11,7 +12,7 @@ void Zeros_Ones(int n,int arr[n][n])
             break;
         }
     }
-    for(int i=0;i<n;i++)
+    for(int i=0;i<n;i++)    //check for zeros in 1st col
     {
         if(arr[i][0]==0)
         {
@@ -20,7 +21,7 @@ void Zeros_Ones(int n,int arr[n][n])
         }
     }
 
-    for(int i=1;i<n;i++)
+    for(int i=1;i<n;i++)    //put 0 in corresponding 1st row,col if find zero
     {
         for(int j=1;j<n;j++)
         {
@@ -31,7 +32,7 @@ void Zeros_Ones(int n,int arr[n][n])
             }
         }
     }
-    for(int i=1;i<n;i++)
+    for(int i=1;i<n;i++)        //put 0 corresponding 1st row 0's
     {
         if(arr[0][i]==0)
         {
@@ -41,7 +42,7 @@ void Zeros_Ones(int n,int arr[n][n])
             }
         }
     }
-    for(int i=1;i<n;i++)
+    for(int i=1;i<n;i++)        //put 0 corresponding 1st col 0's
     {
         if(arr[i][0]==0)
         {
@@ -51,14 +52,14 @@ void Zeros_Ones(int n,int arr[n][n])
             }
         }
     }
-    if(r==0)
+    if(r==0)                    //if 0 put all zeros,if 1 leave as it is
     {
         for(int i=0;i<n;i++)
         {
             arr[0][i]=0;
         }
     }
-    else if(c==0)
+    else if(c==0)                //if 0 put all zeros,if 1 leave as it is
     {
         for(int i=0;i<n;i++)
         {
